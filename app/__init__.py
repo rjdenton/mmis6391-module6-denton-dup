aa88bdc5d5b68f671f785ced2bd2652f8125377f
rjdenton <riley.denton@gcsu.edu>
2024-11-18 14:24:17 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,21 +5,11 @@ from .db_connect import close_db, get_db
 app = create_app()
 app.secret_key = 'your-secret'  # Replace with an environment variable
 
-<<<<<<< HEAD
-from app.blueprints.users import users
-from app.blueprints.recipes import recipes
-from app.blueprints.favorites import favorites
-
-app.register_blueprint(users)
-app.register_blueprint(recipes)
-app.register_blueprint(favorites)
-=======
 from app.blueprints.sales import sales
 from app.blueprints.regions import regions
 
 app.register_blueprint(sales)
 app.register_blueprint(regions)
->>>>>>> c3681a6c0a71549933b44a1402b765fd210b9af7
 
 from . import routes
 
